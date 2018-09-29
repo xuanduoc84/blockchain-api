760c984b13f8926ab34327748ecc0d5dc0591823
Roller Davic <rollerdavic@gmail.com>
2018-09-29 22:29:52 +0700

    Update

--- application/controllers/Ethereum.php
+++ application/controllers/Ethereum.php
@@ -60,11 +60,11 @@ class Ethereum extends REST_Controller {
 
 	public function wallet_get(){
 		$password = "ConBo";
-		$web3 =  new Web3(new HttpProvider(new HttpRequestManager('http://127.0.0.1:8545', 0.5)));
+		$web3 = $this->connect();
 
 		//print_r($bitcoin->getnewaddress("Test"));
 		$web3->personal->newAccount($password, function ($err, $account){
-			
+			print_r($account);
 			//$this->wallet = $account;
 			$arv = [
 				"status" => (trim($account) ? "success" : "error"),
