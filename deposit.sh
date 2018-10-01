39c2943bc18ec9b19f0e4d44b728911508d46bf0
Roller Davic <rollerdavic@gmail.com>
2018-10-02 02:14:27 +0700

    Update

--- deposit.sh
+++ deposit.sh
@@ -1,8 +1,8 @@
 while [ 1 ]
 do
-  php index.php ethereum deposit
-  #php index.php blockchian deposit
+  #php index.php ethereum deposit
+  php index.php blockchian deposit
   #php index.php tokens deposit
   #php index.php cr9 deposit
-  sleep 3600s
+  sleep 300s
 done
